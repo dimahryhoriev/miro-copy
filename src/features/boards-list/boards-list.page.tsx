8b49dd4c37d22b18adc430976f8b76d444ad5055
Dmytro Hryhoriev <dimag4888@gmail.com>
2026-05-22 15:25:48 +0300

    chore: create global CONFIG hub for env and app settings

--- src/features/boards-list/boards-list.page.tsx
+++ src/features/boards-list/boards-list.page.tsx
@@ -1,10 +1,11 @@
+import { CONFIG } from "@/shared/model/config";
 import { ROUTES } from "@/shared/model/routes";
 import { Link, href } from "react-router-dom";
 
 function BoardsListPage() {
     return (
         <div>
-            <h1>Boards list</h1>
+            <h1>Boards list {CONFIG.API_BASE_URL}</h1>
 
             <Link to={href(ROUTES.BOARD, { boardId: '1' })}>Board 1</Link>
         </div>
