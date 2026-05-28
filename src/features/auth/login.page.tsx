92b63f909b44b7029c32bd0381b3cfe8cae42aa1
Dmytro Hryhoriev <dimag4888@gmail.com>
2026-05-28 23:35:58 +0300

    feat(auth): add AuthLayout and implement Login and Register pages

--- src/features/auth/login.page.tsx
+++ src/features/auth/login.page.tsx
@@ -1,5 +1,21 @@
+import { Link } from "react-router-dom";
+import { AuthLayout } from "./auth-layout";
+import { ROUTES } from "@/shared/model/routes";
+
 function LoginPage() {
-    return <div>Login page</div>
+    return (
+        <AuthLayout
+            title='Log In'
+            description='Enter your email and password to log in'
+            form={<form></form>}
+            footerText={
+                <>
+                    {"Don't have an account?"} <Link to={ROUTES.REGISTER}>Sign Up</Link>
+                </>
+            }
+        >
+        </AuthLayout>
+    )
 }
 
 export const Component = LoginPage;
