a38ef9ce4cd3cbd5a6e828c8a08b79a462401998
Dmytro Hryhoriev <dimag4888@gmail.com>
2026-08-08 18:12:57 +0300

    wip: add static SVG Arrow component

--- src/features/board/board.page.tsx
+++ src/features/board/board.page.tsx
@@ -17,6 +17,7 @@ import { Overlay } from "./ui/overlay";
 import { Sticker } from "./ui/sticker";
 import { useNodesDimensions } from "./hooks/use-nodes-dimensions";
 import { useWindowPositionModel } from "./model/window-position";
+import { vectorFromPoints, type Point } from "./domain/point";
 
 function BoardPage() {
     const nodesModel = useNodes();
@@ -80,6 +81,16 @@ function BoardPage() {
                         {...viewModel.selectionWindow}
                     />
                 )}
+                <Arrow
+                    start={{
+                        x: 30,
+                        y: 50,
+                    }}
+                    end={{
+                        x: 90,
+                        y: 90,
+                    }}
+                />
             </Canvas>
             <Actions>
                 <ActionButton
@@ -100,3 +111,70 @@ function BoardPage() {
 }
 
 export const Component = BoardPage;
+
+function Arrow({
+    start,
+    end,
+}: {
+    start: Point;
+    end: Point;
+}) {
+    const diff = vectorFromPoints(
+        start,
+        end,
+    );
+    const angle = Math.atan2(
+        diff.y,
+        diff.x,
+    );
+    const arrowRightAngle = angle + Math.PI * (1 - 1 / 6);
+    const arrowLeftAngle = angle - Math.PI * (1 - 1 / 6);
+    const arrowRightDiff = [
+        Math.cos(arrowRightAngle) * 10,
+        Math.sin(arrowRightAngle) * 10,
+    ];
+    const arrowLeftDiff = [
+        Math.cos(arrowLeftAngle) * 10,
+        Math.sin(arrowLeftAngle) * 10,
+    ];
+
+    return (
+        <svg
+            className="
+                absolute left-0 top-0
+                pointer-events-none
+            "
+        >
+            <path
+                className="
+                    pointer-events-auto
+                    hover:stroke-blue-500
+                    hover:fill-blue-500
+                    transition-[stroke,fill]
+                    duration-300
+                "
+                stroke="black"
+                strokeWidth={2}
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                fill="black"
+                d={
+                    `
+                    M ${start.x} ${start.y}
+                    L ${end.x} ${end.y}
+                    L
+                    ${end.x + arrowRightDiff[0]}
+                    ${end.y + arrowRightDiff[1]}
+                    L
+                    ${end.x + (-5 * Math.cos(angle))}
+                    ${end.y + (-5 * Math.sin(angle))}
+                    L
+                    ${end.x + arrowLeftDiff[0]}
+                    ${end.y + arrowLeftDiff[1]}
+                    L ${end.x} ${end.y}
+                    `
+                }
+            />
+        </svg>
+    );
+};
